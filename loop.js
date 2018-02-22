1a05608b0522c0fa71c6b200d9937349438102f8
Mona Lisa <limn64@yahoo.com>
2018-02-21 20:58:56 -0800

    add all

--- loop.js
+++ loop.js
@@ -1,8 +1,7 @@
 const inputStr = prompt('Please input a count: ')
 const inputCount = parseInt(inputStr, 10)
 document.write("Starting Loop" + "<br />")
-for(let count = 0; count < inputCount; count++) 
-{ 
+for(let count = 0; count < inputCount; count++) { 
     document.write("Current Count : " + count ) 	
     document.write("<br />"); 
 } 
