dd554a896be435c8951f74a64be197666748a78d
Mona Lisa <limn64@yahoo.com>
2018-02-24 09:35:16 -0800

    add all

--- website/end.js
+++ website/end.js
@@ -1,3 +1,3 @@
 const message = prompt('Enter the end:')
-const output = document.getElementById('output')
+const output = document.getElementById('output2')
 output.innerHTML = 'Your message is: ' + message
