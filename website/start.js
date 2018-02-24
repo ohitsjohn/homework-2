31f6819efa23182df08d1205112515113fdcc4b3
Mona Lisa <limn64@yahoo.com>
2018-02-24 09:56:30 -0800

    add all

--- website/start.js
+++ website/start.js
@@ -1,3 +1,11 @@
 const message = prompt('Enter the start:')
 const output = document.getElementById('output')
 output.innerHTML = 'Your message is: ' + message
+
+const message = prompt('Enter the start:')
+const output = document.getElementById('output')
+output.innerHTML = 'Your message is: ' + message
+
+const message = prompt('Enter the start:')
+const output = document.getElementById('output')
+output.innerHTML = 'Your message is: ' + message
