bbb60afa4b0941215502ac27f693e6700069d1ee
Mona Lisa <limn64@yahoo.com>
2018-02-27 13:44:55 -0800

    add all

---
+++ website/error.js
@@ -0,0 +1,37 @@
+var s = document.getElementById("start").value;
+    
+var e = document.getElementById("end").value;
+
+var i = document.getElementById("inc").value;
+
+var s1 = parseInt(s);
+
+var e1 = parseInt(e);
+
+var i1 = parseInt(i);
+
+var x;
+
+var loop = [];
+
+var loop1 = [];
+
+var i = 0;
+
+if(typeof s == 'number'){
+   document.write(num1 + " is a number <br/>");
+}else{
+   document.write(num1 + " is not a number <br/>");
+}
+
+if(typeof e == 'number'){
+    document.write(num1 + " is a number <br/>");
+}else{
+    document.write(num1 + " is not a number <br/>");
+}
+
+if(typeof i == 'number'){
+    document.write(num1 + " is a number <br/>");
+}else{
+    document.write(num1 + " is not a number <br/>");
+}
