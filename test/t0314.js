d8d5ccd75b10fa6e357c7dc162fdcc2814a2e99f
Mona Lisa <limn64@yahoo.com>
2018-03-14 21:33:27 -0700

    add all

--- test/t0314.js
+++ test/t0314.js
@@ -38,7 +38,7 @@ const output = document.getElementById('output')
 addBtn.addEventListener('click', () => {
     const val1 = num1.value
     const val1 = num2.value
-    const sum = val1 + val2
+    const sum = parseInt(val1, 2) + parseInt(val2, 2)
     output.innerHTML = sum
-    
+
 })
