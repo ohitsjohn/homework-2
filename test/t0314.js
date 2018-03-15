0ddf0e99c1e2fbff8b636b4f10fbc22934e4ee94
Mona Lisa <limn64@yahoo.com>
2018-03-14 21:02:41 -0700

    add all

--- test/t0314.js
+++ test/t0314.js
@@ -1,12 +1,21 @@
 const btn = document.querySelectory('#myButton')
 
-function addLang() {
-    console.log('addLang called')
+function addLang(e) {
+    console.log('addLang called ${e}')
     const el=document.createElement('li')
     el.innerHTML='C++'
 
     const languages = document.getElementById('languages')
     languages.appendChild(el) 
+    e.stopPropagation()
 }
 
-btn.addEventListener('click', addLang
+btn.addEventListener('click', addLang)
+
+
+function mouseEventHandler(e) {
+    const mouseInfo = 'click location:  X ${e.clienX}  Y ${e.clientY}'
+    console.log(mouseInfo)
+}
+
+document.addEventListener('click', mouseEventHandler)
