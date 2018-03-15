3e8699134162f820393336e9d9a09a98f303cf30
Mona Lisa <limn64@yahoo.com>
2018-03-14 20:55:32 -0700

    add all

--- test/t0314.js
+++ test/t0314.js
@@ -1,10 +1,9 @@
-
 const btn = document.querySelectory('#myButton')
 
 function addLang() {
     console.log('addLang called')
     const el=document.createElement('li')
-    el.innerHTML='c++'
+    el.innerHTML='C++'
 
     const languages = document.getElementById('languages')
     languages.appendChild(el) 
