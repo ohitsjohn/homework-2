0d31c0f1f7deb075c1a6aefcf6663e4d1079054d
Mona Lisa <limn64@yahoo.com>
2018-03-14 19:54:17 -0700

    add all

---
+++ start.js
@@ -0,0 +1,3 @@
+const message = prompt('Enter the start:')
+const output = document.getElementById('output')
+output.innerHTML = 'Your message is: ' + message
---
+++ step.js
@@ -0,0 +1,3 @@
+const message = prompt('Enter the step:')
+const output = document.getElementById('output')
+output.innerHTML = 'Your message is: ' + message
