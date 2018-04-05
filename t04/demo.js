ba778e6f4cd372ddf685a70db075dbd2fdf48172
Mona Lisa <limn64@yahoo.com>
2018-04-04 20:00:17 -0700

    add all

---
+++ t04/demo.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+	console.log( "ready!" );
+});   
+// shortcut
+$(() => {
+	console.log( "ready!" );
+});
+
+
+// $document is ready
+$(myFunction)
+
+function myFunction() {
+    console.log("document is ready 222!");
+}
