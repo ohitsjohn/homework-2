29c5dee621cfb076bebb0c3f2f3b967c29818e07
Mona Lisa <limn64@yahoo.com>
2018-04-04 21:28:58 -0700

    add all

--- t04/demo.js
+++ t04/demo.js
@@ -12,4 +12,25 @@ $(myFunction)
 
 function myFunction() {
     console.log("document is ready 222!");
+
+//  $('input').focus(function() {
+//      $(this).css('background', 'green')
+//  })
+
+    $('input').focus(onInputFocus)
+
+    $('input').blur(onInputBlur)
+}
+
+
+function moveHandler(e) {
+    $('#coords').html('Coords X: ' + e.clientX + ' Y: ' + e.clientY)
+}
+
+function onInputFocus(e) {
+    $(e.target).css('background', 'pink')
+}
+
+function onInputBlur(e) {
+    $(e.target).css('background', 'white')
 }
